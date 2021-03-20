9e14d58ec12482dff138796e84d1fe75605a7828
CeTo <ctroncoso@imed.cl>
2021-03-20 15:54:00 -0300

    add validation days

--- scripts/pvp.js
+++ scripts/pvp.js
@@ -1,17 +1,30 @@
 const moment = require('moment')
 require('moment-timezone');
 
-module.exports = function(robot) {
-  robot.respond(/pr(?:ó|o)ximo pvp/gi, function(msg){
+const formatSaturday = (data) => {
+  const fridayFormat = data.format().split('T')[0];
+  return moment(fridayFormat+' 00:00:00')
+};
+
+const diferenceResponse = (intial, final) => {
+  return moment.duration(intial.diff(final));
+};
+
+module.exports = function (robot) {
+  robot.respond(/pr(?:ó|o)ximo pvp/gi, async function(msg){
     const nextFriday = moment().day('Saturday');
-    const fridayFormat = nextFriday.format().split('T')[0];
-    const fridayformatToCompare = moment(fridayFormat+' 00:00:00')
+    const fridayformatToCompare = await formatSaturday(nextFriday);
     const today = moment.tz('America/Santiago');
-    const diferences = moment.duration(fridayformatToCompare.diff(today));
-    console.log(diferences.days());
+    let diferences = diferenceResponse(fridayformatToCompare, today)
+
+    if(diferences.hours() < 0) {
+      const nextFridayWeek = formatSaturday(nextFriday.add(1, 'weeks')) 
+      diferences = diferenceResponse(nextFridayWeek, today)
+    }
+
     if (diferences.days() === 0) {
       return msg.send(`Queda ${diferences.hours()} Horas - ${diferences.minutes()} Minutos`)
-    }
+    } 
     return msg.send(`Queda ${diferences.days()} Dias - ${diferences.hours()} Horas - ${diferences.minutes()} Minutos`)
   });
 }
